337a4489326e1ceb6f1189407075915e0e093239
Zachary Boelter <zboelter@gmail.com>
2024-04-03 13:38:27 -0700

    initial commit. start structuring

---
+++ src/components/Hangman.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import data from '../data.json'
+
+const Hangman = () => {
+
+    return (
+        <div className="game-container">
+            
+        </div>
+    )
+}
+
+export default Hangman
