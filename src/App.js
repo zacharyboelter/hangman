17f17d5fadacea8060c704ec1feb5a268537db0b
Zachary Boelter <zboelter@gmail.com>
2024-04-03 15:07:57 -0700

    state setup App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Hangman from "./components/Hangman";
 
 
+
+
 const App = () => {
+  const [wordToGuess, setWordToGuess] = useState('')
+  const [guessedLetters, setGuessedLetters] = useState([])
+  const [incorrectGuesses, setIncorrectGuesses] = useState(0)
+
+
   return (
-    <div className='app'>
+    <div className='game-container'>
       <Hangman />
     </div>
   )
