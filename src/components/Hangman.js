3292db44bb178cda439d62370749b4bdcc9df3a9
Zachary Boelter <zboelter@gmail.com>
2024-04-04 15:45:50 -0700

    imports for logic

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
 import data from '../data.json'
+import Controls from './Controls'
+import Health from './Health'
+import Keyboard from './Keyboard'
+import Letters from './Letters'
+import Word from './Word'
+
 
 const Hangman = () => {
 
